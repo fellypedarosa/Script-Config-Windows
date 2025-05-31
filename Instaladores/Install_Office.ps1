7ef2b285ab0803bb3a27ac17e1bcf24fdaf84707
fellypedarosa <fellypedarosa@icloud.com>
2025-05-31 13:57:39 -0300

    Install Office Minimalista. Funciona no terminal mas não no script

--- Instaladores/Install_Office.ps1
+++ Instaladores/Install_Office.ps1
@@ -1,1 +1,8 @@
-Write-Host "Executando Office.ps1"
+$officePath = "C:\SCRIPT 2.0\Dependencias\office"
+$setupExe = Join-Path $officePath "setup.exe"
+$configXml = "configuration.xml"  # Caminho relativo ao diretório de trabalho
+
+Start-Process -FilePath $setupExe `
+    -ArgumentList "/configure $configXml" `
+    -WorkingDirectory $officePath `
+    -Wait -NoNewWindow
