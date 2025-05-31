645b33457ca8be8aeb314f165be51a7843696571
fellypedarosa <fellypedarosa@icloud.com>
2025-05-31 15:17:30 -0300

    Instalando Softwares Padroes

--- Instaladores/Install_Softwares.ps1
+++ Instaladores/Install_Softwares.ps1
@@ -2,36 +2,36 @@ Write-Host "`n-- INSTALACAO DE SOFTWARES (SEQUENCIAL) --" -ForegroundColor Cyan
 
 # Take Control
 Write-Host "Instalando Take Control..."
-#$path = "C:\SCRIPT 2.0\Dependencias\Take_Control\Take_Control.msi"
-#Start-Process "msiexec.exe" -ArgumentList "/i `"$path`" /qn /norestart" -Wait
+$path = "C:\SCRIPT 2.0\Dependencias\Take_Control\Take_Control.msi"
+Start-Process "msiexec.exe" -ArgumentList "/i `"$path`" /qn /norestart" -Wait
 Write-Host "Take Control instalado."
 
 # Java
 Write-Host "Instalando Java..."
-#winget install --id=EclipseAdoptium.Temurin.17.JRE --source winget -e --silent
+winget install --id=EclipseAdoptium.Temurin.17.JRE --source winget -e --silent
 
 # Chrome
 Write-Host "Instalando Google Chrome..."
-#winget install --id=Google.Chrome --source winget -e --silent
+winget install --id=Google.Chrome --source winget -e --silent
 
 # Adobe
 Write-Host "Instalando Adobe Acrobat Reader..."
-#winget install --id=Adobe.Acrobat.Reader.64-bit --source winget -e --silent
+winget install --id=Adobe.Acrobat.Reader.64-bit --source winget -e --silent
 
 # 7-Zip
 Write-Host "Instalando 7-Zip..."
-#winget install --id=7zip.7zip --source winget -e --silent
+winget install --id=7zip.7zip --source winget -e --silent
 
 # VLC
 Write-Host "Instalando VLC Media Player..."
-#winget install --id=VideoLAN.VLC --source winget -e --silent
+winget install --id=VideoLAN.VLC --source winget -e --silent
 
 # PDFsam Basic
 Write-Host "Instalando PDFsam..."
-#winget install --id=PDFsam.PDFsam --source winget -e --silent
+winget install --id=PDFsam.PDFsam --source winget -e --silent
 
 # Firefox
 Write-Host "Instalando Mozilla Firefox..."
-#winget install --id=Mozilla.Firefox --source winget -e --silent
+winget install --id=Mozilla.Firefox --source winget -e --silent
 
 Write-Host "`nTodos os programas foram instalados." -ForegroundColor Green
